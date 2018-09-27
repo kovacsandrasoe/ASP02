7fb34e32f8b3ae401ffa8719b7ed6868df7b8003
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2018-09-27 17:51:48 +0200

    #10: ModelBinder kész

---
+++ ASP02/Data/WorkerModelBinder.cs
@@ -0,0 +1,49 @@
+﻿using ASP02.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP02.Data
+{
+    public class WorkerModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext ==null)
+            {
+                throw new ArgumentException(nameof(bindingContext));
+            }
+
+            var name = bindingContext.ValueProvider.GetValue("name");
+            var job = bindingContext.ValueProvider.GetValue("job");
+            var salary = bindingContext.ValueProvider.GetValue("salary");
+
+            WorkerModel model = new WorkerModel()
+            {
+                Name = name.FirstValue,
+                Job = job.FirstValue,
+                Salary = int.Parse(salary.FirstValue)
+            };
+
+            if (model.Salary < 300000)
+            {
+                model.SalaryCategory = SalaryCategories.Low;
+            }
+            else if (model.Salary < 600000)
+            {
+                model.SalaryCategory = SalaryCategories.Middle;
+            }
+            else
+            {
+                model.SalaryCategory = SalaryCategories.High;
+            }
+
+            bindingContext.Result = 
+                ModelBindingResult.Success(model);
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- ASP02/Models/WorkerModel.cs
+++ ASP02/Models/WorkerModel.cs
@@ -1,14 +1,23 @@
-﻿using System;
+﻿using ASP02.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ASP02.Models
 {
+    public enum SalaryCategories
+    {
+        Low, Middle, High
+    }
+
+    [ModelBinder(BinderType = typeof(WorkerModelBinder))]
     public class WorkerModel
     {
         public string Name { get; set; }
         public string Job { get; set; }
         public int Salary { get; set; }
+        public SalaryCategories SalaryCategory { get; set; }
     }
 }
